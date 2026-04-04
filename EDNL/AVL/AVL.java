e9de065de96f4f8fe7e5b95c3543b89fa8b7f8be
Fernanda Dantas do Nascimento <110307073+fwrnandaaa@users.noreply.github.com>
2026-04-04 23:55:57 +0000

    Implementações de métodos de rotação

--- EDNL/AVL/AVL.java
+++ EDNL/AVL/AVL.java
@@ -11,27 +11,60 @@ public class AVL extends ArvoreBinaria {
         Node current = O.right;
         current.parent = O.parent;
         if (root == O){
-            root = current
+            root = current;
         }
         else{
             O.parent.right = current;
         }
         current.left = O;
         O.parent = current;
-
+        current.fb = current.fb + 1 - Math.min(O.fb, 0);
+        O.fb = O.fb + 1 + Math.max(current.fb, 0);
        }
-    }
-    public void rotacaDireita(Node O){
-       return;
-    }
+    
+    public void rotacaoDireita(Node O){
+        Node current = O.left;
+        current.parent = O.parent;
+        if (root == O){
+            root = current;
+        }
+        else{
+            O.parent.left = current;
+        }
+        current.right = O;
+        O.parent = current;
+        current.fb = current.fb - 1 - Math.max(O.fb, 0);
+        O.fb = O.fb - 1 + Math.min(current.fb, 0);
+       }
+    
     public void duplaEsquerda(Node O){
-       return;
+      rotacaoDireita(O.right);
+      rotacaoEsquerda(O);
     }
-    public void duplaEsquerda(Node O){
-       return;
+    public void duplaDireita(Node O){
+       rotacaoEsquerda(O.left);
+       rotacaoDireita(O);
     }
     public void rebalancear(Node O){
-       return;
+       if(O.fb >= -1 && O.fb <= 1){
+        return;
+       }
+       if(O.fb == 2){
+        if(O.right.fb <= 0){
+            rotacaoEsquerda(O);
+        }
+        if(O.right.fb > 0){
+            duplaEsquerda(O);
+        }
+       }
+       if(O.fb == -2){
+        if(O.left.fb >= 0){
+            rotacaoDireita(O);
+        }
+        if(O.left.fb < 0){
+            duplaDireita(O);
+        }
+       }
     }
     @Override
     public void insert(Node O){
